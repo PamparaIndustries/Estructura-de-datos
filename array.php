96e105055ab87b2649cfddb1a5b79629d7476f0d
sebasp640 <sebasp640@hotmail.com>
2025-03-18 10:30:26 -0500

    repo 1 Arreglos

--- array.php
+++ array.php
@@ -1,3 +1,22 @@
 <?php
-	echo "Hello, World!"
+
+$marcas = array("Toyota", "Ford", "BMW", "Honda");
+
+$marcas[] = "Chevrolet";
+
+print_r($marcas);
+
+// Eliminar un registro de un arreglo
+$eliminarMarca = "Ford";
+$llave = array_search($eliminarMarca, $marcas);
+
+if ($llave !== false) {
+    unset($marcas[$llave]);
+}
+
+// Reindexar el arreglo para evitar claves vacías
+$marcas = array_values($marcas);
+
+print_r($marcas);
+
 ?>
---
+++ modificararray.php
@@ -0,0 +1,13 @@
+<?php
+
+$marcas = array("Toyota", "Ford", "BMW", "Honda");
+
+$marcas[] = "Chevrolet";
+
+print_r($marcas);
+
+$marcas[1] = "Tesla";
+
+print_r($marcas);
+
+?>
